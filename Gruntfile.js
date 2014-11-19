c7772b3b27aea41fff0898a94dd4a5720e7e5a5d
Cecile Jardin <cjardin@kilix.fr>
2014-11-19 18:10:48 +0100

    End project

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,24 +13,48 @@ module.exports = function(grunt) {
         },
         files: {
           'css/app.css': 'scss/app.scss',
-          'css/menu.css': 'scss/menu.scss'
         }        
       }
     },
+
+    cssmin: {
+        Dist: {
+          files: {
+            'css/app.min.css': ['css/app.css']
+          }
+        }
+    },
+
+    uglify: {
+    dist: {
+      files: {
+        'js/app.min.js': ['!js/gruntfile.js','js/app.js', 'js/menu.js', 'js/slider.js']
+      }
+    }
+  },
 
     watch: {
       grunt: { files: ['Gruntfile.js'] },
 
       sass: {
         files: 'scss/**/*.scss',
-        tasks: ['sass']
+        tasks: ['cssroutine']
+      },
+
+      js: {
+        files: 'js/**/*.js',
+        tasks: ['jsroutine']
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('build', ['sass']);
-  grunt.registerTask('default', ['build','watch']);
+  grunt.registerTask('cssroutine', ['sass', 'cssmin']);
+  grunt.registerTask('jsroutine', ['uglify']);
+  grunt.registerTask('default', ['cssroutine', 'jsroutine', 'watch']);
+
 }
--- js/slider.js
+++ js/slider.js
@@ -1,28 +1,82 @@
-sliderInt=1;
-sliderNext=2;
-
 $(document).ready(function(){
-    $('#slider > img#1').fadeIn(300);
-    startSlider();
-});
+    
+    var $slider = $('#slider'), // Ciblage bloc slider
+        $img = $('#slider img'), // Ciblage des balise img du slider
+        indexImg = $img.length - 1, // Définir l'index
+        i = 0, // Initilise le compteur
+        $currentImg = $img.eq(i); // Ciblage de l'image courante (index à 0)
 
-function startSlider(){
-    count=$('#slider>img').size();
+    /*$img.css('display', 'none'); //
+    $currentImg.css('display', 'block'); //*/
+
+    $img.addClass("fadeout"); // Cacher les images
+    $currentImg.addClass( "fadein" ); // Afficher seulement image courante
 
 
 
-    loop=setInterval(function(){
+    $('.next').click(function(){ 
 
-        if(sliderNext>count){
-        sliderNext=1;
-        sliderInt=1;
+        i++; // incrémente le compteur
+        $img.addClass("fadeout").removeClass("fadein"); // on cache les images
+
+        if( i > indexImg ){
+            i = 0;
         }
 
-        $('#slider > img').fadeOut(300);
-        $('#slider > img#'+sliderNext).fadeIn(300);
+        $currentImg = $img.eq(i); // on définit la nouvelle image
+        $currentImg.addClass("fadein").removeClass("fadeout"); // puis on l'affiche
 
-        sliderInt=sliderNext;
-        sliderNext=sliderNext+1;
+    });
 
-    }, 3000)
-};
+    $('.prev').click(function(){
+
+        i--; // Décrémente le compteur
+        $img.addClass("fadeout").removeClass("fadein");
+
+        if( i < 0 ){
+            
+            i = indexImg;
+        }
+
+        $currentImg = $img.eq(i);
+        $currentImg.addClass("fadein").removeClass("fadeout");
+    });
+
+    $('.dots').click(function() {
+
+        var numDot = $(this).attr( "id" );
+            /*console.log(numDot);*/
+        var elem = numDot.split('dot');
+        i= elem[1];
+        i= i - 1 ;
+
+        /*i = 0;*/
+        $img.addClass("fadeout").removeClass("fadein");
+        $currentImg = $img.eq(i);
+        $currentImg.addClass("fadein").removeClass("fadeout");
+
+    });
+
+    function slideImg(){
+        setTimeout(function(){
+                            
+            if(i < indexImg){ // si le compteur est inférieur au dernier index
+            i++; // on l'incrémente
+        }
+        else{ // sinon, on le remet à 0 (première image)
+            i = 0;
+        }
+
+        $img.addClass("fadeout").removeClass("fadein");
+
+        $currentImg = $img.eq(i);
+        $currentImg.addClass("fadein").removeClass("fadeout");
+
+        slideImg();
+
+        }, 7000);
+    }
+
+    slideImg();
+
+});
